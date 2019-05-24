36fa00051d3664f120261d696211a6798ee511d5
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-24 16:05:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,16 +56,7 @@ class Owner
     end
   end
   
-  # def sell_pets
-  #   pets.each do |species, animals|
-  #     animals.each do |animal|
-  #       animal.mood= "nervous"
-  #     end
-  #     animals.clear
-  #   end
-  # end
-  
-    def sell_pets
+  def sell_pets
     pets.each do |species, animals|
       animals.each do |animal|
         animal.mood = "nervous"
